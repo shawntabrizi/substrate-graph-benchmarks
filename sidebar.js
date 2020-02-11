f968ea3148c2520eb1b3ded4a5f494547cc42ba9
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-12 00:24:17 +0100

    Add read_value benchmark

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,14 @@
 async function createSidebar() {
-  let metas = ['./pallet-balances/meta.json', './pallet-balances-lazy-reaping/meta.json', './pallet-identity/meta.json'];
-  for (meta of metas) {
-    let json = await d3.json(meta);
+  let folders = [
+    'pallet-benchmark',
+    'pallet-balances',
+    'pallet-balances-lazy-reaping',
+    'pallet-identity'
+  ];
+  for (folder of folders) {
+    let meta = '/' + folder + '/' + 'meta.json';
 
-    let folder = meta.split('/');
+    let json = await d3.json(meta);
 
     let sidebar = document.getElementById('sidebar');
     let h6 = document.createElement('h6');
@@ -17,7 +22,7 @@ async function createSidebar() {
       'mb-1',
       'text-muted'
     );
-    h6.innerText = folder[1];
+    h6.innerText = folder;
 
     let ul = document.createElement('ul');
     ul.classList.add('nav', 'flex-column', 'mb-2');
@@ -27,10 +32,10 @@ async function createSidebar() {
       li.classList.add('nav-item');
       let a = document.createElement('a');
       a.classList.add('nav-link');
-      a.href = '?p=' + folder[1] + '&e=' + item;
+      a.href = '?p=' + folder + '&e=' + item;
       a.innerText = item;
       a.onclick = function() {
-        parseCsv('./' + folder[1] + '/' + this.innerText + '.txt');
+        parseCsv('/' + folder + '/' + this.innerText + '.txt');
       };
 
       li.appendChild(a);
