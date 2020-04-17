bd09b6f95adc67c5ecc873dd5c9eeb9ed4a918e9
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-04-17 12:37:16 +0200

    use file stream

--- util/clean.js
+++ util/clean.js
@@ -1,57 +1,74 @@
 // Main function which needs to run at start
 async function main() {
-	var args = process.argv;
+    var args = process.argv;
 
-	const fs = require('fs');
-	const path = require('path');
+    const fs = require('fs');
+    const readline = require('readline');
+    const path = require('path');
 
-	// This gets the filename from commandline
-	let dirname = path.join(__dirname, args[2]);
+    // This gets the filename from commandline
+    let dirname = path.join(__dirname, args[2]);
 
-	fs.readdir(dirname, function (err, filenames) {
-		if (err) throw err;
+    fs.readdir(dirname, function(err, filenames) {
+        if (err) throw err;
 
-		filenames.forEach(function (filename) {
-			let filepath = path.join(__dirname, args[2], filename);
+        filenames.forEach(function(filename) {
+            let in_filepath = path.join(__dirname, args[2], filename);
+            let out_filepath = path.join(__dirname, args[2], "temp" + filename);
 
-			let lines_saved = false;
-			let lines_deleted = false;
+            const instream = fs.createReadStream(in_filepath);
+            const outputFile = fs.createWriteStream(out_filepath);
 
-			fs.readFile(filepath, { encoding: 'utf-8' }, function (err, data) {
-				if (err) throw err;
-				let dataArray = data.split('\n'); // convert file data in an array
+            // Track when lines should be saved while processing stream
+            let saveLine = false;
+            // Track if the final file should be saved
+            let saveFile = false;
 
-				let length = dataArray.length;
+            const rl = readline.createInterface({
+                input: instream,
+            });
 
-				for (let i = length - 1; 0 <= i; i--) {
-					if (!(dataArray[i].includes("End Benchmark"))) {
-						dataArray.splice(i, 1);
-						lines_deleted = true;
-					} else {
-						while (!(dataArray[i].includes("Start Benchmark"))) {
-							i--;
-						}
-						// Skip the start benchmark line
-						// This file seems to be correct to clean up, so we will save it
-						if (i >= 0) {
-							lines_saved = true;
-						}
-					}
-				}
+            // Handle any error that occurs on the write stream
+            outputFile.on('err', err => { console.error(err) })
 
+            // Once done writing, rename the output to be the input file name
+            outputFile.on('close', () => {
+                if (saveFile) {
+                    fs.rename(out_filepath, in_filepath, err => {
+                        if (err) { console.error(err) }
+                    })
+                    console.log('Successfully updated the file data');
+                } else {
+                    // Remove temp file
+                    fs.unlink(out_filepath, function(err) {
+                        console.log("No updates");
+                    });
+                }
+            })
 
-				if (lines_saved && lines_deleted) {
-					console.log("Saving File")
-					const updatedData = dataArray.join('\n');
-					fs.writeFile(filepath, updatedData, (err) => {
-						if (err) throw err;
-						console.log('Successfully updated the file data');
-					});
+            // Read the file and replace any text that matches
+            rl.on('line', line => {
+                if (line.includes("Start Benchmark")) {
+                    // Start saving lines
+                    saveLine = true;
+                    saveFile = true;
+                } else if (line.includes("End Benchmark") && saveLine) {
+                    // Write this final line
+                    outputFile.write(`${line}\n`)
+                    saveLine = false;
+                }
 
-				}
-			});
-		});
-	});
+                if (saveLine) {
+                    outputFile.write(`${line}\n`)
+                }
+            })
+
+            // Done reading the input, call end() on the write stream
+            rl.on('close', () => {
+                outputFile.end()
+            })
+        });
+    });
 }
 
 main()
