0d57cb628b0114fb7e42c4e91e5955c700b4c433
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-05-07 19:38:22 +0200

    Update summarize

--- util/summarize.js
+++ util/summarize.js
@@ -5,16 +5,19 @@ async function main() {
     const fs = require('fs');
     const path = require('path');
 
+    const metadata = require('../metadata.json')
+
     // This gets the filename from commandline
     let dirname = path.join(__dirname, args[2]);
 
-    let summarized_data = []
+    let summarized_data = {}
 
     fs.readdir(dirname, function(err, filenames) {
         if (err) throw err;
 
         filenames.forEach(function(filename) {
             let in_filepath = path.join(__dirname, args[2], filename);
+            filename = filename.slice(0, -4);
             if (in_filepath.substr(-3) == 'txt') {
                 let text = fs.readFileSync(in_filepath, "utf8");
                 let text_split = text.split('\n\n');
@@ -24,16 +27,24 @@ async function main() {
                 let analysis_data = text_split;
                 // Mean squares analysis is 5, this creates an array of the data
                 let msa = analysis_data[5].replace(/\s+/g, ' ').trim().split(' ');
-                let extrinsic_data = [];
-                extrinsic_data.push(filename);
+
+                summarized_data[filename] = {};
                 for (let i = 3; i < msa.length; i += 3) {
-                    extrinsic_data.push(msa[i]);
+                    if (i == 3) {
+                        summarized_data[filename]["base"] = msa[i];
+                    } else {
+                        let component_letter = msa[i - 1];
+                        // let pallet = filename.split('_')[0];
+                        // summarized_data[filename][component_letter] = {}
+                        // summarized_data[filename][component_letter]["meta"] = metadata[pallet][component_letter] || "?";
+                        // summarized_data[filename][component_letter]["time"] = msa[i];
+                        summarized_data[filename][component_letter] = msa[i];
+                    }
                 }
-                summarized_data.push(extrinsic_data)
             }
         });
-
-        fs.writeFileSync("../summary.csv", summarized_data.join('\n'));
+        console.log(summarized_data)
+        fs.writeFileSync("../summary.json", JSON.stringify(summarized_data, null, '\t'));
     });
 
 }
