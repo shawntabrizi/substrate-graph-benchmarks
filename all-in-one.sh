49e27a6b09738c3584cc26457d102c713a8bfcf6
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-06-08 18:11:08 +0200

    benchmark data for indices

--- all-in-one.sh
+++ all-in-one.sh
@@ -170,6 +170,14 @@
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet im-online --extrinsic validate_unsigned --raw --execution=wasm --wasm-execution=compiled > ./output/im-online_validate_unsigned.txt
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet im-online --extrinsic validate_unsigned_and_then_heartbeat --log state=trace,benchmark=trace 2> ./output/im-online_validate_unsigned_and_then_heartbeat.log
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet im-online --extrinsic validate_unsigned_and_then_heartbeat --raw --execution=wasm --wasm-execution=compiled > ./output/im-online_validate_unsigned_and_then_heartbeat.txt
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet indices --extrinsic claim --log state=trace,benchmark=trace 2> ./output/indices_claim.log
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet indices --extrinsic claim --raw --execution=wasm --wasm-execution=compiled > ./output/indices_claim.txt
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet indices --extrinsic transfer --log state=trace,benchmark=trace 2> ./output/indices_transfer.log
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet indices --extrinsic transfer --raw --execution=wasm --wasm-execution=compiled > ./output/indices_transfer.txt
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet indices --extrinsic free --log state=trace,benchmark=trace 2> ./output/indices_free.log
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet indices --extrinsic free --raw --execution=wasm --wasm-execution=compiled > ./output/indices_free.txt
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet indices --extrinsic force_transfer --log state=trace,benchmark=trace 2> ./output/indices_force_transfer.log
+./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet indices --extrinsic force_transfer --raw --execution=wasm --wasm-execution=compiled > ./output/indices_force_transfer.txt
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet offences --extrinsic report_offence_im_online --log state=trace,benchmark=trace 2> ./output/offences_report_offence_im_online.log
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 50 --repeat 20 --pallet offences --extrinsic report_offence_im_online --raw --execution=wasm --wasm-execution=compiled > ./output/offences_report_offence_im_online.txt
 ./polkadot/target/release/polkadot benchmark --chain kusama-dev --steps 5 --pallet offences --extrinsic report_offence_grandpa --log state=trace,benchmark=trace 2> ./output/offences_report_offence_grandpa.log
