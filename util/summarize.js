cdb4604929600940ac4e9d96808135492f16dc58
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-07-01 15:00:16 +0200

    add db to summarize

--- util/summarize.js
+++ util/summarize.js
@@ -27,6 +27,10 @@ async function main() {
                 let analysis_data = text_split;
                 // Mean squares analysis is 5, this creates an array of the data
                 let msa = analysis_data[5].replace(/\s+/g, ' ').trim().split(' ');
+                // DB data is 6, this splits all numbers and text
+                let db = analysis_data[6].replace(/[^a-zA-Z0-9]/g, ' ').split(' ').filter(x => x);
+
+                console.log(db)
 
                 summarized_data[filename] = {};
                 for (let i = 3; i < msa.length; i += 3) {
@@ -39,6 +43,18 @@ async function main() {
                         // summarized_data[filename][component_letter]["meta"] = metadata[pallet][component_letter] || "?";
                         // summarized_data[filename][component_letter]["time"] = msa[i];
                         summarized_data[filename][component_letter] = msa[i];
+                    }
+                }
+                summarized_data[filename]["db"] = {}
+                // Symmetrical amount of data for reads and writes
+                let half_length = db.length / 2;
+                for (let i = 0; i < half_length; i += 2) {
+                    if (i == 0) {
+                        summarized_data[filename]["db"]["reads"] = { "base": db[1] };
+                        summarized_data[filename]["db"]["writes"] = { "base": db[1 + half_length] };
+                    } else {
+                        summarized_data[filename]["db"]["reads"][db[i + 1]] = db[i];
+                        summarized_data[filename]["db"]["writes"][db[i + 1 + half_length]] = db[i + half_length];
                     }
                 }
             }
