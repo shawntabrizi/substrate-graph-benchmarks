eeac5283af52cc6a29264691dca6ed647e6b1afb
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-11-03 14:53:17 +0100

    update generate

--- util/generate_script.js
+++ util/generate_script.js
@@ -21,30 +21,34 @@ async function main() {
         }
         //let extrinsic_name = line.extrinsic;
         // Options
-        let name = "substrate";
-        //let name = "polkadot";
+        //let name = "substrate";
+        let name = "polkadot";
         let executable = "./" + name + "/target/release/" + name;
         let benchmark = "benchmark";
         //let chain = "--chain dev"
-        let chain = "--chain dev";
+        let chain = "--chain=polkadot-dev";
         let execution = "--execution=wasm --wasm-execution=compiled";
         //let logging = "--log state=trace,benchmark=trace";
-        let steps_full = "--steps 50";
+        let steps_full = "--steps=50";
         //let steps_small = "--steps 5";
-        let repeat = "--repeat 20";
-        let pallet = "--pallet " + pallet_name;
+        let repeat = "--repeat=20";
+        let pallet = "--pallet=" + pallet_name;
         //let extrinsic = "--extrinsic " + extrinsic_name;
-        let extrinsic = '--extrinsic "*"';
-        let raw = "--raw";
+        let extrinsic = '--extrinsic="*"';
+        //let raw = "--raw";
+        let raw = "";
         //let output_log = "2> ./output/" + pallet_name + "_" + extrinsic_name + ".log";
-        let output = `--output ./${name}/bin/node/runtime/src/weights`;
-        let header = `--header ./${name}/HEADER`;
+        //let output = `--output ./${name}/bin/node/runtime/src/weights`;
+        let output = "--output=./runtime/polkadot/src/weights/";
+        //let header = `--header ./${name}/HEADER`;
+        let header = "--header=./file_header.txt";
+        let heap_pages = "--heap-pages=4096";
         //let output_data = "> ./output/" + pallet_name + "_" + extrinsic_name + ".txt";
 
         // let final_log = [executable, benchmark, chain, steps_small, pallet, extrinsic, logging, output_log].join(" ");
         // all_script.push(final_log)
 
-        let final_data = [executable, benchmark, chain, steps_full, repeat, pallet, extrinsic, raw, execution, header, output].join(" ");
+        let final_data = [executable, benchmark, chain, steps_full, repeat, pallet, extrinsic, raw, execution, heap_pages, header, output].join(" ");
         all_script.push(final_data)
     }
 
